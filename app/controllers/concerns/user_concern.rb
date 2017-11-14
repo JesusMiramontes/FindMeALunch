0bf8af60cfaf42eb125a256eb7cd2025171a616e
Jesus <jesus_miramontes@me.com>
2017-11-14 18:57:06 +0000

    Concern, verifies if the user is admin

---
+++ app/controllers/concerns/user_concern.rb
@@ -0,0 +1,13 @@
+module UserConcern
+    extend ActiveSupport::Concern
+    
+    def only_admins
+      unless is_admin?
+        redirect_to products_path
+      end
+    end
+    
+    def is_admin?
+        Integer(current_user.access_lvl) < 2
+    end
+end
